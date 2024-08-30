9159f780a3cc2e94da8be399b400248c2d89844d
AimTzy-cloud <aimtzy18@gmail.com>
2024-08-30 11:20:47 +0700

    menambahkan file

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/* LIGHT AND DARK MODE */
+
+const /** {NodeElement} */ $themeBtn = document.querySelector("[data-theme-btn]");
+const /** {NodeElement} */ $HTML = document.documentElement;
+let /** {Boolean | String} */ isDark = window.matchMedia("(prefers-color-scheme)").matches;
+
+if (sessionStorage.getItem("theme")) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+const changeTheme = () => {
+    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" :
+    "light";
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+$themeBtn.addEventListener("click", changeTheme);
+
+/* TAB */
+const /** {NodeList} */ $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let /** {NodeElement} */ [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let /** {NodeElement} */ [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item => {
+    item.addEventListener("click", function () {
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active");
+
+        const /** {NodeElement} */ $tabContent = document.querySelector(`
+        [data-tab-content="${item.dataset.tabBtn}"]`);
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+
+        lastActiveTab = $tabContent;
+        lastActiveTabBtn = this
+    });
+});
